a001c7b19ce58041b6617ba6c2eefc52155bcf0a
Cody D'Orazio <cxdorazio@gmail.com>
2020-02-13 17:17:57 -0500

    Commit Log: (02.13.20): Initial Commit

---
+++ cmake-build-debug/CMakeFiles/atm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atm.dir/main.c.obj"
+  "atm.exe"
+  "atm.exe.manifest"
+  "atm.lib"
+  "atm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/atm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void start();
+void printIntroMenu();
+void printMainMenu();
+void createAccount();
+void login();
+
+//char menuInput;
+
+int main() {
+    //printf("Hello, World!\n");
+    // calls upon my "Start" function.
+    start();
+    return 0;
+}
+
+// Start Function
+void start() {
+
+}
+
+void printIntroMenu() {
+
+}
+
+void printMainMenu() {
+
+}
+
+void createAccount() {
+
+}
+
+void login() {
+
+}
