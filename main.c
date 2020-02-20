63e4d5db1f9a2e33acb9bf007488bc3dca856528
Cody D'Orazio <cxdorazio@gmail.com>
2020-02-20 13:01:02 -0500

    Commit Log: (02.13.20): Initial Commit (02.19.20): Initialized new functions. (02.19.20): Finished functions. (ERRORS ARE PRESENT)

--- main.c
+++ main.c
@@ -6,51 +6,132 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int choice, pin, k;
 
-char name[] = "nana";
-int balance = 5000;
 int pin = 3014;
+int choice;
+int curbalance = 5000;
+int withdrawAmount = 0;
+int deposited = 0;
+int amountWithdrawn = 0;
+int pinGuess = 0;
+//int withdrawAmount = 0;
+int transactions = 0;
+//int deposited = 0;
+int todayDeposit = 0; // this will allow only 3 deposits once integrated.
 
 
-int main() {
+char name[5] = {'N', 'a', 'n', 'a', '\0'};
 
-    switch(choice) {
-        case 1: balance();
-            break;
+void balance();
 
-        case 2: withdraw();
-        break;
+void withdraw();
 
-        case 3: deposit();
-            break;
+void deposit();
 
-        case 4: quit();
-            break;
+void quit();
 
-        default: printf("Please try again and select a number from 1 - 4");
-    }
+void newBalance(int *balance);
+void newDeposit(int *deposit);
+void newWithdrawnAmount (int *withdrawnAmount);
 
+
+int main(void) {
+
+    do {
+        if (pinGuess != pin  && pinGuess !=0)
+            printf("Wrong pin. Please try again.\n");
+        printf("Enter your pin -> \n");
+        scanf_s("%d", &pinGuess,3); //
+    } while (pinGuess != pin);
+
+    do {
+        printf("Welcome %s\n", name);
+        printf("\nPlease make a selection.\n");
+        printf("1. Check Balance\n");
+        printf("2. Withdraw Cash\n");
+        printf("3. Deposit Cash\n");
+        printf("4. Quit\n");
+        printf("***********************************************\n");
+        printf("What would you like to do?\t");
+        scanf_s("%d", &choice, 20);
+
+        /* Switch and cases. */
+        switch (choice) {
+            case 1:
+                balance();
+                break;
+
+            case 2:
+                withdraw();
+                break;
+
+            case 3:
+                deposit();
+                break;
+
+            case 4:
+                quit();
+                break;
+
+            default:
+                printf("Invalid selection. Please try again!");
+        }
+    } while (choice != 4);
+    //++ transactions;
 }
 
+/* Case 1*/
 void balance() {
-    printf("$%d", balance);
+    printf("\nYour current balance is: $%d\n", curbalance);
 
 }
 
+/* Case 2*/
 void withdraw() {
+    do {
+        if (amountWithdrawn > 1000 || withdrawAmount > 1000) {
+            printf("You have reached your withdraw limit for the day!\n");
+        }
+        if (withdrawAmount % 20 != 0) {
+            printf("Please try again, you can only withdraw in amounts of $20\n");
+        }
+        if (withdrawAmount > curbalance) {
+            printf("You do not have enough funds in your account.\n");
+        }
 
+        printf("How much would you like to withdraw?\n");
+        scanf_s("%d", &withdrawAmount, 16); /*TODO: added 14 for the size of the 4 bit array. */
 
+    } while (amountWithdrawn > 1000 || withdrawAmount % 20 != 0 || withdrawAmount > curbalance ||
+             withdrawAmount > 1000);
+    //withdrawAmount == *newWithdrawnAmount();
+    curbalance -= amountWithdrawn;
+    amountWithdrawn += withdrawAmount;
+
+    ++transactions;
 }
 
+
+/* Case 3*/
 void deposit() {
 
+    do {
+        if (deposited > 10000) {
+            printf("You have reached your deposit limit for the day!\n");
+        }
+        printf("How much would you like to deposit?\n");
+        scanf_s("%d", &deposited);
+    } while (deposited > 10000);  // Deposit limit for the day
+    curbalance += deposited;    // Current balance = current balance + deposited
+     ++transactions;
 
 }
 
-int quit() {
+/* Case 4*/
+void quit() {
+    printf("You have completed %d transactions\n", transactions);
+    printf("Have a nice day!\n");
 
 
-
-    // exit(0);
+    exit(0);
 }
